38fb41c2c1506f32b5fd868655d708b03de66e02
Piyyaaa <apriyantojordy7@gmail.com>
2025-06-30 00:54:06 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import App from 'src/App.jsx';
 import Navbar from './components/navbar.jsx';
 import 'remixicon/fonts/remixicon.css'
 import 'animate.css';
