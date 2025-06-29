407b40ab6d1671e38cf6bd34a94427011cd4f980
Jordy Apriyanto <jordy.535210073@stu.untar.ac.id>
2025-06-30 00:43:19 +0700

    fix: case-sensitive import issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './app.jsx';
+import App from './App.jsx';
 import Navbar from './components/navbar.jsx';
 import 'remixicon/fonts/remixicon.css'
 import 'animate.css';
