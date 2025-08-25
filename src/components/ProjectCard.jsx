424667b6e32f8e73dd511e6ada6ef1c01e213ebf
Piyyaaa <apriyantojordy7@gmail.com>
2025-08-25 13:47:11 +0700

    Update ProjectCard.jsx
    Updated ProjecCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,10 @@
-// components/ProjectCard.jsx
-import { FiExternalLink } from "react-icons/fi"; // Pastikan kamu install react-icons
+import { FiExternalLink } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 
 function ProjectCard({ gambar, nama, shortDesk, tools, delay, slug }) {
   return (
     <div
-      className="p-4 bg-zinc-800 rounded-xl shadow-md hover:shadow-2xl hover:shadow-blue-700/40 transform hover:-translate-y-1 transition-all duration-300"
+      className="p-4 bg-zinc-800 rounded-xl shadow-md hover:shadow-2xl hover:shadow-blue-700/40 transform hover:-translate-y-1 transition-all duration-300 flex flex-col justify-between min-h-[550px]"
       data-aos="fade-up"
       data-aos-duration="2000"
       data-aos-delay={delay}
@@ -34,7 +33,7 @@ function ProjectCard({ gambar, nama, shortDesk, tools, delay, slug }) {
           ))}
         </div>
 
-        <div className="text-center" id="1">
+        <div className="mt-auto text-center">
           <Link
             to={`/project/${slug}`}
             className="inline-flex items-center justify-center gap-2 w-full bg-blue-700 hover:bg-blue-600 text-white py-2 rounded-lg border border-zinc-600 transition duration-300"
